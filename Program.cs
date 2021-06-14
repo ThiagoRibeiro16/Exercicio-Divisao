d8421954e3fc015de89294f58bca62dc96ea2498
Thiago Ribeiro <thiago.escola321@gmail.com>
2021-06-14 04:34:21 -0300

    Finaliza o programa

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercicio_Divisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double numerador , denominador , resultado; 
+
+            Console.WriteLine("Simulador de divisão\n");
+
+            Console.Write("Digite o numerador....: ");
+           numerador = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("\nDigite o denominador..: ");
+            denominador = Convert.ToDouble(Console.ReadLine());
+
+            if (numerador > 0 && denominador > 0)
+            {
+                resultado =  numerador / denominador;
+
+               Console.WriteLine($"\n{numerador} dividido por {denominador} é {resultado}");
+             }
+            
+            else
+            {
+               Console.WriteLine("\nNão é possível dividir por zero");
+               
+            }
+
+                    Console.WriteLine("\nObrigado por utilizar nosso simulador");
+        }
+
+    }
+}
